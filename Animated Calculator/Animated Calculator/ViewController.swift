83853a56b4537f9fadd6be5899dd883bc50e7426
Harsha Goli <harshagoli@Harshas-MacBook-Pro.local>
2017-05-23 13:30:58 -0400

    Created storyboard screen and added outlets

---
+++ Animated Calculator/Animated Calculator/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Animated Calculator
+//
+//  Created by Harsha Goli on 5/23/17.
+//  Copyright © 2017 Harsha Goli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button0: UIButton!
+
+    @IBOutlet weak var buttonDecimal: UIButton!
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBOutlet weak var button5: UIButton!
+    
+    @IBOutlet weak var button7: UIButton!
+    
+    @IBOutlet weak var button6: UIButton!
+    
+    @IBOutlet weak var button8: UIButton!
+    
+    @IBOutlet weak var button9: UIButton!
+    
+    @IBOutlet weak var buttonClearScreen: UIButton!
+    
+    @IBOutlet weak var buttonDivide: UIButton!
+    
+    @IBOutlet weak var ButtonMultiply: UIButton!
+    
+    @IBOutlet weak var ButtonEquals: UIButton!
+    
+    @IBOutlet weak var ButtonPlus: UIButton!
+    
+    @IBOutlet weak var ButtonMinus: UIButton!
+    
+    @IBOutlet weak var ButtonPercentage: UIButton!
+    
+    @IBOutlet weak var ButtonDelete: UIButton!
+    
+    @IBOutlet weak var OutputScreen: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
