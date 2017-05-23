1eb38dec4a545d564da17c9696e93d16e96769b8
Harsha Goli <harshagoli@Harshas-MacBook-Pro.local>
2017-05-23 13:58:09 -0400

    Added action logic for all the number buttons

--- Animated Calculator/Animated Calculator/ViewController.swift
+++ Animated Calculator/Animated Calculator/ViewController.swift
@@ -48,6 +48,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var ButtonDelete: UIButton!
     
     @IBOutlet weak var OutputScreen: UILabel!
+    
+    var stack:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -58,6 +61,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Mark Actions
+    @IBAction func PressedZero(_ sender: Any) {
+        stack.append("0")
+    }
+    @IBAction func PressedOne(_ sender: Any) {
+        stack.append("1")
+    }
+    @IBAction func PressedTwo(_ sender: Any) {
+        stack.append("2")
+    }
+    @IBAction func Pressed3(_ sender: Any) {
+        stack.append("3")
+    }
+    @IBAction func PressedFour(_ sender: Any) {
+        stack.append("4")
+    }
+    @IBAction func PressedFive(_ sender: Any) {
+        stack.append("5")
+    }
+    @IBAction func PressedSix(_ sender: Any) {
+        stack.append("6")
+    }
+    @IBAction func PressedSeven(_ sender: Any) {
+        stack.append("7")
+    }
+    @IBAction func PressedEight(_ sender: Any) {
+        stack.append("8")
+    }
+    @IBAction func PressedNine(_ sender: Any) {
+        stack.append("9")
+    }
+    
+    @IBAction func PressedDecimal(_ sender: Any) {
+        if(stack.contains(".")){
+            return
+        }
+        else{
+            stack.append(".")
+        }
+    }
+    
+    
+    
 }
 
